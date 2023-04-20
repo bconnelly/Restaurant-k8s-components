13e6a3d7fdc756e14b5c78a95aba1b94c8532c7b
Bryan <bryan.p.connelly@gmail.com>
2023-04-20 11:23:45 -0600

    debugging random failures (000s, 404s)

--- tests.sh
+++ tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 LOAD_BALANCER=$1
-#CUSTOMER_NAME=$(($RANDOM % 1000))
 CUSTOMER_NAME=$(tr -cd "[:digit:]" < /dev/urandom | head -c 6)
 
 SEAT_CUSTOMER_VALID_FAILS=0
@@ -77,7 +76,7 @@ SUBMIT_ORDER_MISSING_PARAM_FAILS=0
 for value in 1 2 3
 do
   echo "Pinging /submitOrder with missing param, should return 400..."
-  response=$(curl -X POST -s -w -iv --raw "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/submitOrder?firstName=$CUSTOMER_NAME&tableNumber=5&dish=burg")
+  response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/submitOrder?firstName=$CUSTOMER_NAME&tableNumber=5&dish=burg")
   echo $response
   if [ $response -eq 400 ]; then break; else SUBMIT_ORDER_MISSING_PARAM_FAILS=$((SUBMIT_ORDER_MISSING_PARAM_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
