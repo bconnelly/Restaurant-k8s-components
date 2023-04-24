cda3f0e2c7c9324df6111ebca43064b5d513260f
Bryan <bryan.p.connelly@gmail.com>
2023-04-24 09:23:40 -0600

    adding back in bootCustomer. minor formatting.

--- tests.py
+++ tests.py
@@ -1,12 +1,10 @@
 import requests
-import json
 import random
-import os
 import sys
 
 LOAD_BALANCER = sys.argv[1]
 SERVICE_PATH = "RestaurantService"
-CUSTOMER_NAME = random.randint(0,10000)
+CUSTOMER_NAME = random.randint(0, 10000)
 
 print("Test customer name: " + str(CUSTOMER_NAME))
 print("First arg: " + str(sys.argv[1]))
@@ -14,7 +12,8 @@ print("First arg: " + str(sys.argv[1]))
 retryCount = 3
 for i in range(retryCount):
     print("Pinging /seatCustomer with valid request...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer", data = {'firstName':CUSTOMER_NAME, 'address':'someaddress','cash':1.23})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer",
+                             data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress', 'cash': 1.23})
     print("response: " + str(response.status_code))
     if response.status_code == 200: break
     # if this was the last try and we didn't get a 200, fail
@@ -22,7 +21,8 @@ for i in range(retryCount):
 
 for i in range(retryCount):
     print("Pinging /seatCustomer with missing param...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer", data = {'firstName':CUSTOMER_NAME, 'address':'someaddress'})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer",
+                             data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress'})
     print("response: " + str(response.status_code))
     if response.status_code == 400: break
     # if this was the last try and we didn't get a 400, fail
@@ -30,13 +30,12 @@ for i in range(retryCount):
 
 for i in range(retryCount):
     print("Pinging /seatCustomer with invalid param...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer", data = {'firstName':CUSTOMER_NAME, 'address':'someaddress','cash':"bad-value"})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer",
+                             data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress', 'cash': "bad-value"})
     print("response: " + str(response.status_code))
     if response.status_code == 400: break
-    # if this was the last try and we didn't get a 200, fail
+    # if this was the last try and we didn't get a 400, fail
     if retryCount == 2: sys.exit(1)
-
-
 
 for i in range(retryCount):
     print("Pinging /getOpenTables with valid request...")
@@ -46,11 +45,10 @@ for i in range(retryCount):
     # if this was the last try and we didn't get a 200, fail
     if retryCount == 2: sys.exit(1)
 
-
-
 for i in range(retryCount):
     print("Pinging /submitOrder with valid request...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/submitOrder", data = {'firstName':CUSTOMER_NAME, 'tableNumber':'1', 'dish':'food', 'bill':1.00})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/submitOrder",
+                             data = {'firstName': CUSTOMER_NAME, 'tableNumber': '1', 'dish': 'food', 'bill': 1.00})
     print("response: " + str(response.status_code))
     if response.status_code == 200: break
     # if this was the last try and we didn't get a 200, fail
@@ -58,17 +56,17 @@ for i in range(retryCount):
 
 for i in range(retryCount):
     print("Pinging /submitOrder with insufficient funds...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/submitOrder", data = {'firstName':CUSTOMER_NAME, 'tableNumber':'1', 'dish':'food', 'bill':100.00})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/submitOrder",
+                             data = {'firstName': CUSTOMER_NAME, 'tableNumber': '1', 'dish': 'food', 'bill': 100.00})
     print("response: " + str(response.status_code))
     if response.status_code == 500: break
-    # if this was the last try and we didn't get a 200, fail
+    # if this was the last try and we didn't get a 500, fail
     if retryCount == 2: sys.exit(1)
-
-
 
 for i in range(retryCount):
     print("Pinging /bootCustomer with valid request...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/bootCustomer", data = {'firstName':CUSTOMER_NAME})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/bootCustomer",
+                             data = {'firstName': CUSTOMER_NAME})
     print("response: " + str(response.status_code))
     if response.status_code == 200: break
     # if this was the last try and we didn't get a 200, fail
@@ -76,8 +74,9 @@ for i in range(retryCount):
 
 for i in range(retryCount):
     print("Pinging /bootCustomer that has already been booted...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/bootCustomer", data={'firstName': CUSTOMER_NAME})
+    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/bootCustomer",
+                             data={'firstName': CUSTOMER_NAME})
     print("response: " + str(response.status_code))
     if response.status_code == 404: break
-    # if this was the last try and we didn't get a 200, fail
+    # if this was the last try and we didn't get a 404, fail
     if retryCount == 2: sys.exit(1)
--- tests.sh
+++ tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 LOAD_BALANCER=$1
-#CUSTOMER_NAME=$(($RANDOM % 1000))
 CUSTOMER_NAME=$(tr -cd "[:digit:]" < /dev/urandom | head -c 6)
 
 SEAT_CUSTOMER_VALID_FAILS=0
@@ -10,33 +9,33 @@ do
   TEST_VAL=$((TEST_VAL+1))
   echo "Pinging /seatCustomer with valid request, should return 200..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/seatCustomer?firstName=$CUSTOMER_NAME&address=mainst&cash=1.23")
-  echo $response
-  if [ $response -eq 200 ]; then break; else SEAT_CUSTOMER_VALID_FAILS=$((SEAT_CUSTOMER_VALID_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 200 ]; then break; else SEAT_CUSTOMER_VALID_FAILS=$((SEAT_CUSTOMER_VALID_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SEAT_CUSTOMER_VALID_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SEAT_CUSTOMER_VALID_FAILS"
 
 SEAT_CUSTOMER_MISSING_PARAM_FAILS=0
 for value in 1 2 3
 do
   echo "Pinging /seatCustomer with missing param, should return 400..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/seatCustomer?firstName=$CUSTOMER_NAME&address=mainst")
-  echo $response
-  if [ $response -eq 400 ]; then break; else SEAT_CUSTOMER_MISSING_PARAM_FAILS=$((SEAT_CUSTOMER_MISSING_PARAM_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 400 ]; then break; else SEAT_CUSTOMER_MISSING_PARAM_FAILS=$((SEAT_CUSTOMER_MISSING_PARAM_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SEAT_CUSTOMER_MISSING_PARAM_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SEAT_CUSTOMER_MISSING_PARAM_FAILS"
 
 SEAT_CUSTOMER_INVALID_PARAM_FAILS=0
 for value in 1 2 3
 do
   echo "Pinging /seatCustomer with invalid param, should return 400..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/seatCustomer?firstName=$CUSTOMER_NAME&address=mainst&cash=bad-value")
-  echo $response
-  if [ $response -eq 400 ]; then break; else SEAT_CUSTOMER_INVALID_PARAM_FAILS=$((SEAT_CUSTOMER_INVALID_PARAM_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 400 ]; then break; else SEAT_CUSTOMER_INVALID_PARAM_FAILS=$((SEAT_CUSTOMER_INVALID_PARAM_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SEAT_CUSTOMER_INVALID_PARAM_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SEAT_CUSTOMER_INVALID_PARAM_FAILS"
 
 
 GET_OPEN_TABLES_FAILS=0
@@ -44,11 +43,11 @@ for value in 1 2 3
 do
   echo "Pinging /getOpenTables..."
   response=$(curl -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/getOpenTables")
-  echo $response
-  if [ $response -eq 200 ]; then break; else GET_OPEN_TABLES_FAILS=$((GET_OPEN_TABLES_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 200 ]; then break; else GET_OPEN_TABLES_FAILS=$((GET_OPEN_TABLES_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $GET_OPEN_TABLES_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$GET_OPEN_TABLES_FAILS"
 
 
 SUBMIT_ORDER_FAILS=0
@@ -56,41 +55,63 @@ for value in 1 2 3
 do
   echo "Pinging /submitOrder with valid request, should return 200..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/submitOrder?firstName=$CUSTOMER_NAME&tableNumber=5&dish=burg&bill=1.00")
-  echo $response
-  if [ $response -eq 200 ]; then break; else SUBMIT_ORDER_FAILS=$((SUBMIT_ORDER_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 200 ]; then break; else SUBMIT_ORDER_FAILS=$((SUBMIT_ORDER_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SUBMIT_ORDER_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SUBMIT_ORDER_FAILS"
 
 SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS=0
 for value in 1 2 3
 do
   echo "Pinging /submitOrder with insufficient funds, should return 500..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/submitOrder?firstName=$CUSTOMER_NAME&tableNumber=5&dish=burg&bill=100.00")
-  echo $response
-  if [ $response -eq 500 ]; then break; else SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS=$((SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 500 ]; then break; else SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS=$((SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SUBMIT_ORDER_INSUFFICIENT_FUNDS_FAILS"
 
 SUBMIT_ORDER_MISSING_PARAM_FAILS=0
 for value in 1 2 3
 do
   echo "Pinging /submitOrder with missing param, should return 400..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/submitOrder?firstName=$CUSTOMER_NAME&tableNumber=5&dish=burg")
-  echo $response
-  if [ $response -eq 400 ]; then break; else SUBMIT_ORDER_MISSING_PARAM_FAILS=$((SUBMIT_ORDER_MISSING_PARAM_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 400 ]; then break; else SUBMIT_ORDER_MISSING_PARAM_FAILS=$((SUBMIT_ORDER_MISSING_PARAM_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SUBMIT_ORDER_MISSING_PARAM_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SUBMIT_ORDER_MISSING_PARAM_FAILS"
 
 SUBMIT_ORDER_BAD_PARAM_FAILS=0
 for value in 1 2 3
 do
   echo "Pinging /submitOrder with bad param, should return 400..."
   response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/submitOrder?firstName=$CUSTOMER_NAME&tableNumber=5&dish=burg&bill=bad-param")
-  echo $response
-  if [ $response -eq 400 ]; then break; else SUBMIT_ORDER_BAD_PARAM_FAILS=$((SUBMIT_ORDER_BAD_PARAM_FAILS+1)); fi
+  echo "$response"
+  if [ "$response" -eq 400 ]; then break; else SUBMIT_ORDER_BAD_PARAM_FAILS=$((SUBMIT_ORDER_BAD_PARAM_FAILS+1)); fi
   if [ $value -eq 3 ]; then exit 1; fi
 done
-echo "failures: $SUBMIT_ORDER_BAD_PARAM_FAILS out of 3 attempts\n"
+printf "failures: %s out of 3 attempts\n" "$SUBMIT_ORDER_BAD_PARAM_FAILS"
+
+BOOT_CUSTOMER_MISSING_PARAM_FAILS=0
+for value in 1 2 3
+do
+  echo "Pinging /bootCustomer with missing param, should return 400..."
+  response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/bootCustomer")
+  echo "$response"
+  if [ "$response" -eq 400 ]; then break; else BOOT_CUSTOMER_MISSING_PARAM_FAILS=$((BOOT_CUSTOMER_MISSING_PARAM_FAILS+1)); fi
+  if [ $value -eq 3 ]; then exit 1; fi
+done
+printf "failures: %s out of 3 attempts\n" "$BOOT_CUSTOMER_MISSING_PARAM_FAILS"
+
+BOOT_CUSTOMER_BAD_PARAM_FAILS=0
+for value in 1 2 3
+do
+  echo "Pinging /bootCustomer with bad param, should return 404..."
+  response=$(curl -X POST -s -w "%{http_code}" --output /dev/null "http://$LOAD_BALANCER/RestaurantService/bootCustomer?firstName=532159876451579643215456359768")
+  echo "$response"
+  if [ "$response" -eq 400 ]; then break; else BOOT_CUSTOMER_BAD_PARAM_FAILS=$((BOOT_CUSTOMER_BAD_PARAM_FAILS+1)); fi
+  if [ $value -eq 3 ]; then exit 1; fi
+done
+printf "failures: %s out of 3 attempts\n" "$BOOT_CUSTOMER_BAD_PARAM_FAILS"
