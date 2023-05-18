993fdddcb846c778f55d79ee84e423aa975a5e5f
Bryan <bryan.p.connelly@gmail.com>
2023-05-18 16:30:07 -0600

    comment/print cleanup

--- tests.py
+++ tests.py
@@ -6,9 +6,6 @@ LOAD_BALANCER = sys.argv[1]
 SERVICE_PATH = "RestaurantService"
 CUSTOMER_NAME = random.randint(0, 10000)
 
-print("Test customer name: " + str(CUSTOMER_NAME))
-print("First arg: " + str(sys.argv[1]))
-
 retryCount = 3
 
 for i in range(retryCount):
@@ -17,7 +14,6 @@ for i in range(retryCount):
                              data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress'})
     print("response: " + str(response.status_code))
     if response.status_code == 400: break
-    # if this was the last try and we didn't get a 400, fail
     if retryCount == 2: sys.exit(1)
 
 for i in range(retryCount):
@@ -26,7 +22,6 @@ for i in range(retryCount):
                              data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress', 'cash': "bad-value"})
     print("response: " + str(response.status_code))
     if response.status_code == 400: break
-    # if this was the last try and we didn't get a 400, fail
     if retryCount == 2: sys.exit(1)
 
 for i in range(retryCount):
@@ -34,7 +29,6 @@ for i in range(retryCount):
     response = requests.get("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/getOpenTables")
     print("response: " + str(response.status_code))
     if response.status_code == 200: break
-    # if this was the last try and we didn't get a 200, fail
     if retryCount == 2: sys.exit(1)
 
 for i in range(retryCount):
@@ -43,7 +37,6 @@ for i in range(retryCount):
                              data = {'firstName': CUSTOMER_NAME, 'tableNumber': '1', 'dish': 'food', 'bill': 100.00})
     print("response: " + str(response.status_code))
     if response.status_code == 500: break
-    # if this was the last try and we didn't get a 500, fail
     if retryCount == 2: sys.exit(1)
 
 for i in range(retryCount):
@@ -52,5 +45,4 @@ for i in range(retryCount):
                              data={'firstName': CUSTOMER_NAME})
     print("response: " + str(response.status_code))
     if response.status_code == 404: break
-    # if this was the last try and we didn't get a 404, fail
     if retryCount == 2: sys.exit(1)
