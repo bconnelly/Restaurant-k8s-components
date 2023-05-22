17da1852b258fed586599ce0ba3ef382019a173e
Bryan <bryan.p.connelly@gmail.com>
2023-05-21 21:38:45 -0600

    stop condition logic fix, comment/print cleanup

--- tests.py
+++ tests.py
@@ -10,39 +10,39 @@ retryCount = 3
 
 for i in range(retryCount):
     print("Pinging /seatCustomer with missing param...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer",
-                             data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress'})
-    print("response: " + str(response.status_code))
+    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/seatCustomer",
+                             data = {"firstName": CUSTOMER_NAME, "address": "someaddress"})
+    print(f"response: {str(response.status_code)}")
     if response.status_code == 400: break
-    if retryCount == 2: sys.exit(1)
+    if i == 2: sys.exit(1)
 
 for i in range(retryCount):
     print("Pinging /seatCustomer with invalid param...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/seatCustomer",
-                             data = {'firstName': CUSTOMER_NAME, 'address': 'someaddress', 'cash': "bad-value"})
-    print("response: " + str(response.status_code))
+    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/seatCustomer",
+                             data = {"firstName": CUSTOMER_NAME, "address": "someaddress", "cash": "bad-value"})
+    print(f"response: {str(response.status_code)}")
     if response.status_code == 400: break
-    if retryCount == 2: sys.exit(1)
+    if i == 2: sys.exit(1)
 
 for i in range(retryCount):
     print("Pinging /getOpenTables with valid request...")
-    response = requests.get("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/getOpenTables")
-    print("response: " + str(response.status_code))
+    response = requests.get(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/getOpenTables")
+    print(f"response: {str(response.status_code)}")
     if response.status_code == 200: break
-    if retryCount == 2: sys.exit(1)
+    if i == 2: sys.exit(1)
 
 for i in range(retryCount):
     print("Pinging /submitOrder with insufficient funds...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/submitOrder",
-                             data = {'firstName': CUSTOMER_NAME, 'tableNumber': '1', 'dish': 'food', 'bill': 100.00})
-    print("response: " + str(response.status_code))
+    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/submitOrder",
+                             data = {"firstName": CUSTOMER_NAME, "tableNumber": "1", "dish": "food", "bill": 100.00})
+    print(f"response: {str(response.status_code)}")
     if response.status_code == 500: break
-    if retryCount == 2: sys.exit(1)
+    if i == 2: sys.exit(1)
 
 for i in range(retryCount):
     print("Pinging /bootCustomer that has already been booted...")
-    response = requests.post("http://" + LOAD_BALANCER + "/" + SERVICE_PATH + "/bootCustomer",
-                             data={'firstName': CUSTOMER_NAME})
-    print("response: " + str(response.status_code))
+    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/bootCustomer",
+                             data={"firstName": CUSTOMER_NAME})
+    print(f"response: {str(response.status_code)}")
     if response.status_code == 404: break
-    if retryCount == 2: sys.exit(1)
+    if i == 2: sys.exit(1)
