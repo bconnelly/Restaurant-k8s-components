9cca0b47cfdbf4e6e445a880e2206b7b532f3058
Bryan <bryan.p.connelly@gmail.com>
2025-02-05 11:51:55 -0700

    refactor: separated request and retry logic from call params

--- tests.py
+++ tests.py
@@ -1,42 +1,65 @@
+import logging
+import string
+import time
+import uuid
+
 import requests
 import random
 import sys
+import logging
+import secrets
+from enum import Enum
 
 LOAD_BALANCER = sys.argv[1]
 SERVICE_PATH = "RestaurantService"
-CUSTOMER_NAME = random.randint(0, 10000)
+
+# random unique name
+CUSTOMER_NAME = str(uuid.uuid4())
 
 retryCount = 3
 
-# RestaurantService endpoints that don't change data
 
-for i in range(retryCount):
-    print("Pinging /seatCustomer with missing param...")
-    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/seatCustomer",
-                             params = {"firstName": CUSTOMER_NAME, "address": "someaddress"})
-    print(f"response: {str(response.status_code)}")
-    if response.status_code == 400: break
-    if i == 2: sys.exit(1)
+class RequestType(Enum):
+    GET = 0
+    POST = 1
+    PUT = 2
+    DELETE = 3
 
-for i in range(retryCount):
-    print("Pinging /seatCustomer with invalid param...")
-    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/seatCustomer",
-                             params = {"firstName": CUSTOMER_NAME, "address": "someaddress", "cash": "bad-value"})
-    print(f"response: {str(response.status_code)}")
-    if response.status_code == 400: break
-    if i == 2: sys.exit(1)
 
-for i in range(retryCount):
-    print("Pinging /getOpenTables with valid request...")
-    response = requests.get(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/getOpenTables")
-    print(f"response: {str(response.status_code)}")
-    if response.status_code == 200: break
-    if i == 2: sys.exit(1)
+def sendRequest(params, endpoint, reqType, expectedCode):
+    print(CUSTOMER_NAME)
+    for i in range(retryCount):
+        logging.debug(f"Sending {reqType} request to {endpoint} with params {params}")
 
-for i in range(retryCount):
-    print("Pinging /bootCustomer that has already been booted...")
-    response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/bootCustomer",
-                             params={"firstName": CUSTOMER_NAME})
-    print(f"response: {str(response.status_code)}")
-    if response.status_code == 404: break
-    if i == 2: sys.exit(1)
+        if reqType == RequestType.GET:
+            response = requests.get(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/{endpoint}", params=params)
+        elif reqType == RequestType.POST:
+            response = requests.post(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/{endpoint}", params=params)
+        elif reqType == RequestType.PUT:
+            response = requests.put(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/{endpoint}", params=params)
+        elif reqType == RequestType.DELETE:
+            response = requests.delete(f"http://{LOAD_BALANCER}/{SERVICE_PATH}/{endpoint}", params=params)
+        else:
+            raise Exception(f"Request type {reqType} not supported")
+
+        if response.status_code == expectedCode:
+            return
+        elif i == 2:
+            logging.debug(f"Expected response code {expectedCode}, got {response.status_code}")
+            raise Exception(f"Expected response code {expectedCode}, got {response.status_code}")
+
+
+def main():
+    # /seatCustomer with missing param
+    sendRequest({"firstName": CUSTOMER_NAME, "address": "someaddress"}, "seatCustomer", RequestType.POST, 400)
+    # /seatCustomer with bad param value
+    sendRequest({"firstName": CUSTOMER_NAME, "address": "someaddress", "cash": "bad-value"}, "seatCustomer", RequestType.POST, 400)
+    # /getOpenTables with valid request
+    sendRequest(None, "getOpenTables", RequestType.GET, 200)
+    # /bootCustomer with customer not in restaurant
+    sendRequest({"firstName": CUSTOMER_NAME}, "bootCustomer", RequestType.POST, 404)
+    time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
